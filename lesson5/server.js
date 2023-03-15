69edc9bb67bff0c3a5cbdaac089e9c9bdeaae242
Frederico Hakamine <frederico.hakamine@gmail.com>
2023-03-15 14:27:14 -0500

    improve error handling in lessons 1 and 5

--- lesson5/server.js
+++ lesson5/server.js
@@ -3,15 +3,16 @@ var app = express();
 app.use(express.json())
 const port = 3002;
 
-app.post('/', function (req, res) {
+//same function for get and post
+app.all('/', function (req, res) {
    request = req.body;
-   // console.log(request);
-   console.log(request.repository.full_name);
-   console.log(request.zen);
+
+   console.log(`=== New GitHub event: ${req.header("X-GitHub-Event")}`);   
+   console.log(`=== GitHub message: ${request.zen} \n\n`);
 
    res.json({ message: "Thank you git" });
 })
 
 app.listen(port, function () {
-   console.log(`Example app listening at ${port}`)
+   console.log(`Webhook service listening at ${port}`)
 })
